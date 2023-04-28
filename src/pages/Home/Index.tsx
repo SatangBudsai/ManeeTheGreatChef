e7f66d28dcfe61646610cbdb7a124b26c103080f
Top <thammanun_a@cmu.ac.th>
2023-04-28 17:29:07 +0700

    remove y

--- src/pages/Home/Index.tsx
+++ src/pages/Home/Index.tsx
@@ -3,7 +3,7 @@ import FectDataItemPrices from '../../api/getItemPrices';
 import { calculateTotalCost } from "../../utility/calculateTotalPriceData";
 
 const Home = () => {
-    const { data: T7_MEAL_OMELETTE, isLoading: y } = FectDataItemPrices('T7_MEAL_OMELETTE', 'Fort%20Sterling');
+    const { data: T7_MEAL_OMELETTE } = FectDataItemPrices('T7_MEAL_OMELETTE', 'Fort%20Sterling');
     const testData = calculateTotalCost("T7_MEAL_OMELETTE", 10, 'Fort%20Sterling');
     console.log(T7_MEAL_OMELETTE);
     console.log("this is All cost", testData);
